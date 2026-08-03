20bedb9c6b1d9d6cf1355d34bef620294ae47e78
Qfather <ke_777@163.com>
2026-08-03 14:03:42 +0800

    修复噪点类型

--- web/src/ui/AdminPanel.tsx
+++ web/src/ui/AdminPanel.tsx
@@ -55,6 +55,8 @@ export default function AdminPanel({
     gradBottom: string
     bgImage: string
     modelPath: string
+    noiseEnabled: boolean
+    noiseOpacity: number
   }
   onPreview?: (partial: Partial<NonNullable<typeof preview>>) => void
 }) {
